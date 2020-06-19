0de24c95c390463c48dbb1d88502af1b1b1c103a
herbjrob <43552696+herbjrob@users.noreply.github.com>
2020-06-19 16:58:44 -0500

    adding all files

---
+++ base-syntax/base-syntax--assignment-problem/src/User/UserOutput.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const userInput = (props) => {
+    return (
+        <div className="UserInput">
+            <p>Just adding some content</p>
+            <p>For this Example</p>
+        </div>
+    )
+};
+
+export default userInput;
